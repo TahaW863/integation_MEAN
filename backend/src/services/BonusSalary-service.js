56a38884f1241698d15aaf77448a8d8f2575e085
yasmeendalgamouni <dalgamouni12@gmail.com>
2021-12-20 20:14:12 +0100

    updated

--- backend/src/services/BonusSalary-service.js
+++ backend/src/services/BonusSalary-service.js
@@ -59,61 +59,6 @@ module.exports = {
         }
     },
 
-    async calculateTheBonusSalary() {
-        let salesManList = [];
-        let evaluationRecordList = []
-
-        salesManList = await SalesMan.find({});
-        evaluationRecordList = await EvaluationRecord.find({});
-        
-        salesManList.forEach(salesMan => {
-            evaluationRecordList.forEach(evl => {
-                let result = SalesMan.findOne({ 'sid': parseInt(evl.sid) })
-                if (result) {
-                    var bonusValue = 0;
-                    evl.productList.forEach(product => {
-                        product.customerList.forEach(customer => {
-                            if (customer.ranking == "1") {
-                                console.log(customer.ranking);
-                                bonusValue = bonusValue + customer.itemsSold * 30;
-                                console.log(bonusValue);
-                            }
-                            else if (customer.ranking == "2") {
-                                console.log(customer.ranking);
-                                bonusValue += customer.itemsSold * 20;
-                                console.log(bonusValue);
-                            }
-                            else if (customer.ranking == "3") {
-                                console.log(customer.ranking);
-                                bonusValue += parseInt(customer.itemsSold) * 10;
-                                console.log(bonusValue);
-                            }
-                        });
-
-                    });
-
-                    console.log("evaluationRecord ", evl);
-                    salesMan.bonusSalary.value = bonusValue;
-                    console.log("bonusSalary value" + salesMan.bonusSalary.value)
-                    salesMan.bonusSalary.year = 2022;
-                    console.log("bonusSalary year" + salesMan.bonusSalary.year)
-
-                    const salesManupdate = {
-                        sid: salesMan.sid,
-                        employeeId: salesMan.employeeId,
-                        name: salesMan.name,
-                        department: salesMan.department,
-                        bonusSalary: salesMan.bonusSalary || null
-                    }
-                    console.log(salesManupdate);
-                    //addBonusSalaryOHRM(salesMan.employeeId,salesMan.bonusSalary);
-                    SalesMan.updateOne({'sid': parseInt(salesMan.sid) }, { $set: salesManupdate });
-                }
-                else {
-                    console.log("no evaluation reocrd");
-                }
-            });
-        });
-    },
+    
 }
 
